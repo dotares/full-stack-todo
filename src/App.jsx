580005378da683e2bffa0bcc62171ed2a1580157
raregiovanni <giovannirare@gmail.com>
2023-05-23 13:18:46 +0530

    remove components and restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import { useState } from "react";
-import Item from "./components/Item";
-import Input from "./components/Input";
 
 function App() {
-    return (
-        <div className="todoApp">
-            <div className="itemsContainer">
-                <Item />
-                <Item />
-                <Item />
-            </div>
-            <div className="inputContainer">
-                <Input />
-            </div>
-        </div>
-    );
+    return <>Hello world</>;
 }
 
 export default App;
--- src/components/Item.jsx
+++
@@ -1,3 +0,0 @@
-export default function Item() {
-    return <div className="todoItem">Content</div>;
-}
