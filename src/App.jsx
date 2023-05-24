d28cc6b5b4fb128e324c659900c7f8375b6d2e51
raregiovanni <giovannirare@gmail.com>
2023-05-24 13:20:27 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import { useState } from "react";
 
-function Item({ value }) {
-    return <li className="todoItem">{value}</li>;
+function Item({ value, completed }) {
+    const handleComplete = (e) => {
+        if (e.target.checked) {
+            completed = true;
+        } else {
+            completed = false;
+        }
+    };
+
+    return (
+        <li className="todoItem">
+            <div className="todoItemContainer">
+                <input onChange={handleComplete} type="checkbox" />
+                <div className="todoValue">{value}</div>
+            </div>
+        </li>
+    );
 }
 
 function Input({ handleChange, handleClick }) {
@@ -17,19 +32,25 @@ function TodoRow({ tasks }) {
     return (
         <ul className="todoRowSection">
             {tasks.map((value, index) => (
-                <Item key={value.id + index} value={value.message} />
+                <Item
+                    key={value.id + index}
+                    completed={tasks.completed}
+                    value={value.message}
+                />
             ))}
         </ul>
     );
 }
 
 function App() {
-    const [tasks, setTasks] = useState([]);
+    var [tasks, setTasks] = useState([]);
     const [message, setMessage] = useState("");
 
     const handleChange = (e) => setMessage(e.target.value);
     const handleClick = () => {
-        setTasks(tasks.concat({ id: 0, message: `${message}` }));
+        setTasks(
+            tasks.concat({ id: 0, message: `${message}`, completed: false })
+        );
     };
 
     return (
