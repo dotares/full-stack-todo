af2f99e018f79221444b90b550dfb43ea7663875
ap2k3 <arish2k3@gmail.com>
2023-06-25 17:15:23 +0530

    add more structure to backend

--- packages/backend/server/server.mjs
+++ packages/backend/server/server.mjs
@@ -5,11 +5,10 @@ import "dotenv/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
