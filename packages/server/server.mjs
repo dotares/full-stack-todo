f756a16dc5b7343f3cf8fff09ae7efe235272df5
ap2k3 <arish2k3@gmail.com>
2023-06-25 12:20:28 +0530

    fix path for react clientside files

--- packages/server/server.mjs
+++ packages/server/server.mjs
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../src")));
+app.use(express.static(path.join(__dirname, "../client/src")));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
