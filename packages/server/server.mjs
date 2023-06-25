6d90f79c32e3e855357c20d05851ec28a4d47900
ap2k3 <arish2k3@gmail.com>
2023-06-25 11:48:53 +0530

    setup express server but cannot get /

---
+++ packages/server/server.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import "dotenv/config";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../src")));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
