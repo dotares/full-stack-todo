68fb0bee96908e02b14ef3ab687101736363cdb4
ap2k3 <arish2k3@gmail.com>
2023-06-25 12:43:00 +0530

    fix path bug

--- packages/server/server.mjs
+++ packages/server/server.mjs
@@ -9,7 +9,9 @@ const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../client/src")));
+app.use(express.static(path.join(__dirname, "../client")));
+
+console.log(path.join(__dirname, "../client"));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
