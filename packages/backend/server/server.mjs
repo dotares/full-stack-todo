7b531e97d59a5bdb2b83ce98574ec32bac0333ba
ap2k3 <arish2k3@gmail.com>
2023-06-25 17:42:10 +0530

    fix different directory .env issue

--- packages/backend/server/server.mjs
+++ packages/backend/server/server.mjs
@@ -1,16 +1,15 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import "dotenv/config";
-
-// require("dotenv").config({ path: "../" });
+import { config } from "dotenv";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const PORT = process.env.PORT || 5050;
-const app = express();
 
-console.log(PORT);
+config({ path: path.join(__dirname, "../.env") });
+
+const PORT = process.env.PORT;
+const app = express();
 
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
