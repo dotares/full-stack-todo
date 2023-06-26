4058476d08e80a9b04fdca91fa8998d6f9bcd15f
ap2k3 <arish2k3@gmail.com>
2023-06-26 18:09:31 +0530

    before adding fontawesome icons add item plus

--- packages/client/src/components/Input.jsx
+++ packages/client/src/components/Input.jsx
@@ -2,19 +2,19 @@ import TodoApp from "./TodoApp";
 
 export default function Input({ change, click }) {
     return (
-        <div className="inputSection bg-slate-800 text-white p-6 flex fixed bottom-0 place-content-center w-full">
-            <div className="inputText px-2 w-3/4">
+        <div className="inputSection bg-gray-800 text-white p-6 flex fixed bottom-0 place-content-center w-full">
+            <div className="inputText px-2 w-2/3">
                 <input
-                    className="textInput bg-slate-300 text-black w-full rounded-xl p-6"
+                    className="textInput bg-gray-300 text-black w-full rounded-xl p-6"
                     onChange={change}
                     type="text"
                     placeholder="Walk the dog..."
                 />
             </div>
-            <div className="inputButton w-1/4 text-center">
+            <div className="inputButton w-1/3 text-center">
                 <button
                     onClick={click}
-                    className="addItemButton bg-slate-700 rounded-xl p-6"
+                    className="addItemButton bg-green-400 text-black font-bold rounded-xl p-6"
                 >
                     Add Item
                 </button>
