43c8513f159353387333ad575a74a3f35c7629fb
ap2k3 <arish2k3@gmail.com>
2023-06-26 17:57:02 +0530

    add colors to the input section

--- packages/client/src/components/Input.jsx
+++ packages/client/src/components/Input.jsx
@@ -2,13 +2,21 @@ import TodoApp from "./TodoApp";
 
 export default function Input({ change, click }) {
     return (
-        <div className="inputSection p-6 flex fixed bottom-0 place-content-center w-full">
-            <div className="inputText w-full ">
-                <input className="textInput p-6" onChange={change} type="text" placeholder="Walk the dog..."/>
+        <div className="inputSection bg-slate-800 text-white p-6 flex fixed bottom-0 place-content-center w-full">
+            <div className="inputText px-2 w-3/4">
+                <input
+                    className="textInput bg-slate-300 text-black w-full rounded-xl p-6"
+                    onChange={change}
+                    type="text"
+                    placeholder="Walk the dog..."
+                />
             </div>
-            <div className="inputButton w-full text-center">
-                <button onClick={click} className="addItemButton p-6">
-                    Add Item +
+            <div className="inputButton w-1/4 text-center">
+                <button
+                    onClick={click}
+                    className="addItemButton bg-slate-700 rounded-xl p-6"
+                >
+                    Add Item
                 </button>
             </div>
         </div>
