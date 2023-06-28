2eb7efdd1ab2bad1a95739e49950b36c0423f570
ap2k3 <arish2k3@gmail.com>
2023-06-28 12:12:01 +0530

    formatted the todoitems

--- packages/client/src/components/Item.jsx
+++ packages/client/src/components/Item.jsx
@@ -2,12 +2,12 @@ import TodoApp from "./TodoApp";
 
 export default function Item({ value, handleDelete }) {
     return (
-        <div id={value.id} className="itemSection flex space-x-8">
+        <div id={value.id} className="itemSection w-full flex p-6 space-x-8">
             <div className="itemCheckboxSection">
                 <input type="checkbox" />
             </div>
-            <div className="itemTextSection">{value.message}</div>
-            <div className="deleteButtonSection">
+            <div className="itemTextSection w-full">{value.message}</div>
+            <div className="deleteButtonSection text-center">
                 <button
                     onClick={() => handleDelete(value.id)}
                     className="deleteButton"
