ea607303f73f98918ac0ea96f59f23ac2d711b37
fibbbonachos <arish2k3@gmail.com>
2023-07-30 00:16:15 +0530

    fix formatting

--- src/client/src/components/TodoApp.jsx
+++ src/client/src/components/TodoApp.jsx
@@ -1,62 +1,62 @@
 import { useState } from "react";
 import Input from "./Input";
 import Item from "./Item";
-import db from "../../../backend/database/connection.js"
+import db from "../../../backend/database/connection.js";
 
 export default function TodoApp() {
-    const [message, setMessage] = useState("");
-    const [tasks, setTasks] = useState([]);
-    const [isChecked, setIsChecked] = useState(false);
+  const [message, setMessage] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [isChecked, setIsChecked] = useState(false);
 
-    const onChange = (event) => {
-        setMessage(event.target.value);
-    };
+  const onChange = (event) => {
+    setMessage(event.target.value);
+  };
 
-    const onClick = () => {
-        setTasks(
-            tasks.concat({
-                id: crypto.randomUUID(),
-                message: `${message}`,
-                completed: false,
-            })
-        );
-        console.log(db)
-    };
-
-    const handleDelete = (id) => {
-        let removedItemList = tasks.filter((task) => task.id != id);
-        setTasks(removedItemList);
-    };
-
-    const handleChecks = () => {
-        setIsChecked(!isChecked);
-    };
-
-    const handleComplete = (id) => {
-        tasks.map((task) => {
-            if (id === task.id) {
-                task.completed = !task.completed;
-                handleChecks();
-            }
-        });
-    };
-
-    return (
-        <>
-            <div className="todoAppSection text-white">
-                <div className="itemRows p-6">
-                    {tasks.map((task) => (
-                        <Item
-                            handleDelete={handleDelete}
-                            handleComplete={handleComplete}
-                            handleCheck={task.completed}
-                            key={crypto.randomUUID()}
-                            value={task}
-                        />
-                    ))}
-                </div>
-                <Input change={onChange} click={onClick} />
-            </div>
-        </>
+  const onClick = () => {
+    setTasks(
+      tasks.concat({
+        id: crypto.randomUUID(),
+        message: `${message}`,
+        completed: false,
+      })
     );
+    console.log(db);
+  };
+
+  const handleDelete = (id) => {
+    let removedItemList = tasks.filter((task) => task.id != id);
+    setTasks(removedItemList);
+  };
+
+  const handleChecks = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const handleComplete = (id) => {
+    tasks.map((task) => {
+      if (id === task.id) {
+        task.completed = !task.completed;
+        handleChecks();
+      }
+    });
+  };
+
+  return (
+    <>
+      <div className="todoAppSection text-white">
+        <div className="itemRows p-6">
+          {tasks.map((task) => (
+            <Item
+              handleDelete={handleDelete}
+              handleComplete={handleComplete}
+              handleCheck={task.completed}
+              key={crypto.randomUUID()}
+              value={task}
+            />
+          ))}
+        </div>
+        <Input change={onChange} click={onClick} />
+      </div>
+    </>
+  );
 }
